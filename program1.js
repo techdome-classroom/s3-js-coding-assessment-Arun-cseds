8680da98a70975b5e31417baf09a733e6703820c
Arun-cseds <arun2529213@gmail.com>
2024-10-07 15:47:53 +0530

    first

--- program1.js
+++ program1.js
@@ -4,7 +4,37 @@
  */
 var isValid = function(s) {
     
-};
+        const stack = [];
+        const matchingBrackets = {
+            ')': '(',
+            '}': '{',
+            ']': '['
+        };
+    
+        for (let char of s) {
+            if (char in matchingBrackets) {
+    
+                const topElement = stack.length > 0 ? stack.pop() : '#';
+                if (topElement !== matchingBrackets[char]) {
+                    return false;
+                }
+            } else {
+                
+                stack.push(char);
+            }
+        }
+    
+
+        return stack.length === 0;
+    };
+    
+    
+    
+   
+
+
+
+    
 
 module.exports = { isValid };
 
