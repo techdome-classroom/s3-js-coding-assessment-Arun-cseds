ca9ff3295469d96e8e1b7c3b85d91c3061fcc59d
Arun-cseds <arun2529213@gmail.com>
2024-10-07 15:52:47 +0530

    second

--- program2.js
+++ program2.js
@@ -3,6 +3,44 @@
  * @return {number}
  */
 var romanToInt = function(s) {
+
+
+    
+    
+        const romanMap = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        };
+    
+        let total = 0; 
+        const length = s.length;
+    
+        for (let i = 0; i < length; i++) {
+            
+            const currentVal = romanMap[s[i]];
+            
+            
+            if (i < length - 1 && currentVal < romanMap[s[i + 1]]) {
+                
+                total -= currentVal;
+            } else {
+                
+                total += currentVal;
+            }
+        }
+    
+        return total;
+
+    
+    
+    
+
+
     
 };
 
