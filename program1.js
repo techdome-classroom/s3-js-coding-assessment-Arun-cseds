86f2334a2e5e76cff86eb1927210161c1473a12e
Arun Tiwari <133372576+Arun-cseds@users.noreply.github.com>
2024-10-07 15:56:44 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -2,32 +2,8 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
-    
-        const stack = [];
-        const matchingBrackets = {
-            ')': '(',
-            '}': '{',
-            ']': '['
-        };
-    
-        for (let char of s) {
-            if (char in matchingBrackets) {
-    
-                const topElement = stack.length > 0 ? stack.pop() : '#';
-                if (topElement !== matchingBrackets[char]) {
-                    return false;
-                }
-            } else {
-                
-                stack.push(char);
-            }
-        }
-    
 
-        return stack.length === 0;
-    };
-    
+     
     
     
    
