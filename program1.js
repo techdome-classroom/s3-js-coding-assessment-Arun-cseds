6a06660f8fd9802316913a3ad25dd4b07c4cf0ab
Arun-cseds <arun2529213@gmail.com>
2024-10-07 16:05:45 +0530

    new2

--- program1.js
+++ program1.js
@@ -12,18 +12,17 @@ var isValid = function(s) {
 
     for (let char of s) {
         if (char in matchingBrackets) {
-            // Pop the top element from the stack, or use '#' if stack is empty
+            
             const topElement = stack.length > 0 ? stack.pop() : '#';
             if (topElement !== matchingBrackets[char]) {
                 return false;
             }
         } else {
-            // Push opening brackets onto the stack
+            
             stack.push(char);
         }
     }
-
-    // Stack should be empty if all parentheses are valid
+    
     return stack.length === 0;
 };
 
